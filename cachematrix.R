257efc37b57c4b3b7ef0548deda8646852fb35bc
cvuono <50888272+cvuono@users.noreply.github.com>
2019-06-01 20:03:03 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The pair of funcitons avoids repetitive calculation of a matrix inverse
+## by establishing a cache of a previously inverted matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix creates the functions to store and retrieve funcitons to a cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  xinv <- NULL
+  set <- function(y) {
+    x<<-y
+    xinv<<-NULL
+  }
+  get <- function() x
+  setinverse<-function(invmat) xinv <<- invmat
+  getinverse<-function() xinv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve retrieves the inverse of a matrix from Cache or calculates it if nothing in Cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  xinv<-x$getinverse()
+  if(!is.null(xinv)){
+    message("getting cached data")
+    return(xinv)
+  }
+  data<-x$get()
+  xinv<-solve(data)
+  x$setinv(xinv)
+  xinv
 }
+       
